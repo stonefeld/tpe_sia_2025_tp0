442cedc7f0f43efa3d2ad2ddf7a0d02b5534f8cb
stonefeld <theo.stanfield@gmail.com>
2025-03-18 18:23:26 -0300

    ej2b

--- ej2a.py
+++ ej2a.py
@@ -1,4 +1,3 @@
-import json
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,49 +5,38 @@ from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 
 factory = PokemonFactory("pokemon.json")
-pokeball = "pokeball"  # Solo usaremos la Pokébola básica
-pokemons = ["jolteon", "caterpie", "snorlax", "onix", "mewtwo"]
-status_effects = [
-    StatusEffect.NONE, StatusEffect.POISON, StatusEffect.BURN, 
-    StatusEffect.PARALYSIS, StatusEffect.SLEEP, StatusEffect.FREEZE
-]
+pokeball = "pokeball"
+pokemons = ["onix", "caterpie"]
+effects = [StatusEffect.NONE, StatusEffect.POISON, StatusEffect.BURN, StatusEffect.PARALYSIS, StatusEffect.SLEEP, StatusEffect.FREEZE]
 results = {}
 
-
 for pokemon_name in pokemons:
-    
     results[pokemon_name] = {}
 
-    for status in status_effects:
-        results[pokemon_name][status.name] = []
+    for effect in effects:
+        results[pokemon_name][effect.name] = []
+        pokemon = factory.create(pokemon_name, 100, effect, 1)
 
-        for _ in range(100): 
-            pokemon = factory.create(pokemon_name, 100, status, 1)  
-            _, capture_rate = attempt_catch(pokemon, pokeball, 0.15)
-            results[pokemon_name][status.name].append(capture_rate)
-
-
-average_rates = {
-    pokemon: {
-        status: np.mean(rates) for status, rates in status_data.items()
-    } for pokemon, status_data in results.items()
-}
+        for _ in range(100):
+            success, _ = attempt_catch(pokemon, pokeball, 0.15)
+            results[pokemon_name][effect.name].append(success)
 
 
-for pokemon, status_data in average_rates.items():
+for pokemon, result_data in results.items():
+    data = {effect: np.mean(r) * 100 for effect, r in result_data.items()}
+
     plt.figure(figsize=(10, 6))
-
-    plt.bar(status_data.keys(), status_data.values(), color=["red", "orange", "yellow", "blue", "purple", "cyan"])
+    plt.bar(data.keys(), data.values(), edgecolor="black", color="skyblue")
 
     plt.xlabel("Estado de Salud")
     plt.ylabel("Probabilidad de Captura")
     plt.title(f"Efecto del Estado de Salud en la Captura de {pokemon.capitalize()}")
 
     plt.grid(axis="y", linestyle="--", alpha=0.7)
-    plt.ylim(0, 1)
+    # plt.ylim(0, 1)
 
-    for i, (status, rate) in enumerate(status_data.items()):
-        plt.text(i, rate, f"{rate:.2f}", ha="center", va="bottom")
+    for i, rate in enumerate(data.values()):
+        plt.text(i, rate, f"{rate:.2f}%", ha="center", va="bottom")
 
     plt.tight_layout()
     plt.show()
---
+++ ej2b.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.catching import attempt_catch
+from src.pokemon import PokemonFactory, StatusEffect
+
+factory = PokemonFactory("pokemon.json")
+pokemons = ["onix", "caterpie"]
+healths = ["1", "0.75", "0.5", "0.25", "0.01"]
+results = {}
+
+for pokemon_name in pokemons:
+    results[pokemon_name] = {}
+
+    for health in healths:
+        results[pokemon_name][health] = []
+        pokemon = factory.create(pokemon_name, 100, StatusEffect.NONE, float(health))
+
+        for _ in range(100):
+            success, _ = attempt_catch(pokemon, "pokeball")
+            results[pokemon_name][health].append(success)
+
+# Graficando los resultados
+for pokemon, result_data in results.items():
+    data = {health: np.mean(r) * 100 for health, r in result_data.items()}
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(data.keys(), data.values(), edgecolor="black", color="skyblue")
+
+    plt.xlabel("Porcentaje de vida")
+    plt.ylabel("Probabilidad de captura")
+    plt.title(f"Efecto de la Vida en la Captura de {pokemon.capitalize()}")
+
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+
+    for i, rate in enumerate(data.values()):
+        plt.text(i, rate, f"{rate:.2f}%", ha="center", va="bottom")
+
+    plt.tight_layout()
+    plt.show()
